a045c418ed80d0c78bf20389ec6c7a5231b2f5cd
Hozyain <qwpo01_0101@mail.ru>
2024-01-02 13:22:12 +0400

    Added

--- src/main/java/com/pavelm/App.java
+++ src/main/java/com/pavelm/App.java
@@ -4,27 +4,38 @@ import com.pavelm.communication.Communication;
 import com.pavelm.config.Config;
 import com.pavelm.entity.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.http.HttpMethod;
 
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class)){
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class)) {
             Communication communication = context.getBean(Communication.class);
 
             List<User> userList = communication.getAllUsers();
+            System.out.println("All users: " + userList);
 
-            User user = new User("James", "Brown", (byte)25);
-//            communication.saveUser(user);
+
+            User user = new User(3L, "James", "Brown", (byte) 25);
             communication.createUser(user);
-
+            System.out.println("User created: " + user);
+            String info2 = communication.performActionAndGetInfo(HttpMethod.POST, user);
 
             user.setName("Thomas");
             user.setLastName("Shelby");
-//            communication.saveUser(user);
             communication.updateUser(user);
+            System.out.println("User updated: " + user);
+            String info3 = communication.performActionAndGetInfo(HttpMethod.PUT, user);
 
             communication.deleteUser(3L);
+            System.out.println("User deleted");
+            String info4 = communication.performActionAndGetInfo(HttpMethod.DELETE, 3L);
+
+
+            System.out.println(info2);
+            System.out.println(info3);
+            System.out.println(info4);
         }
     }
 }
--- src/main/java/com/pavelm/communication/Communication.java
+++ src/main/java/com/pavelm/communication/Communication.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -30,10 +31,15 @@ public class Communication {
         ResponseEntity<List<User>> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, createHttpEntity(), new ParameterizedTypeReference<List<User>>() {});
         List<User> allUsers = responseEntity.getBody();
 
-        saveSessionId(responseEntity.getHeaders());
+        HttpHeaders responseHeaders = responseEntity.getHeaders();
+        String contentType = responseHeaders.getFirst(HttpHeaders.CONTENT_TYPE);
+        System.out.println("Content-Type header: " + contentType);
+
+        saveSessionId(responseHeaders);
 
         return allUsers;
     }
+
 //    public User getUser(Long id){
 //        User user = restTemplate.getForObject(URL + "/" + id, User.class);
 //        return user;
@@ -53,7 +59,6 @@ public class Communication {
     /*---------------------------------------------*/
     public void createUser(User user){
         HttpHeaders headers = createHttpHeadersWithSessionId();
-        Long id = user.getId();
         restTemplate.exchange(URL, HttpMethod.POST, new HttpEntity<>(user, headers), String.class);
     }
     public void updateUser(User user){
@@ -63,10 +68,17 @@ public class Communication {
 
     public void deleteUser(Long id) {
         HttpHeaders headers = createHttpHeadersWithSessionId();
-        headers.setContentType(MediaType.APPLICATION_JSON);
 
-        restTemplate.exchange(URL + "/" + id, HttpMethod.DELETE, new HttpEntity<>(headers), String.class);
+        User userToDelete = new User();
+        userToDelete.setId(id);
+
+        try {
+            restTemplate.exchange(URL, HttpMethod.DELETE, new HttpEntity<>(userToDelete, headers), String.class);
+        } catch (HttpClientErrorException e) {
+            System.out.println("Error: " + e.getRawStatusCode() + "\n" + e.getResponseBodyAsString());
+        }
     }
+
 
     private HttpHeaders createHttpHeadersWithSessionId() {
         HttpHeaders headers = new HttpHeaders();
@@ -94,4 +106,31 @@ public class Communication {
         }
         return new HttpEntity<>(headers);
     }
+    public String performActionAndGetInfo(HttpMethod method, User user) {
+        HttpHeaders headers = createHttpHeadersWithSessionId();
+        HttpEntity<User> httpEntity = new HttpEntity<>(user, headers);
+
+        ResponseEntity<String> responseEntity;
+        try {
+            responseEntity = restTemplate.exchange(URL, method, httpEntity, String.class);
+        } catch (HttpClientErrorException e) {
+            return "HTTP Status: " + e.getRawStatusCode() + "\nError: " + e.getResponseBodyAsString();
+        }
+
+        return "HTTP Status: " + responseEntity.getStatusCode() + "\nResponse Body: " + responseEntity.getBody();
+    }
+    public String performActionAndGetInfo(HttpMethod method, Long id) {
+        HttpHeaders headers = createHttpHeadersWithSessionId();
+        HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> responseEntity;
+        try {
+            responseEntity = restTemplate.exchange(URL + "/" + id, method, httpEntity, String.class);
+        } catch (HttpClientErrorException e) {
+            return "HTTP Status: " + e.getRawStatusCode() + "\nError: " + e.getResponseBodyAsString();
+        }
+
+        return "HTTP Status: " + responseEntity.getStatusCode() + "\nResponse Body: " + responseEntity.getBody();
+    }
+
 }
